bc446fc3aaff9b23c3c78d8b5da53966ea790c1f
cladams0203 <cladams0203@gmail.com>
2020-09-17 13:49:31 -0400

    finished the user service impl

--- src/main/java/com/lambda/watermyplants/services/UserServiceImpl.java
+++ src/main/java/com/lambda/watermyplants/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lambda.watermyplants.services;
 
+import com.lambda.watermyplants.exceptions.ResourceNotFoundException;
 import com.lambda.watermyplants.models.Plant;
 import com.lambda.watermyplants.models.Role;
 import com.lambda.watermyplants.models.User;
@@ -22,6 +23,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private HelperFunction helperFunction;
 
     @Override
     public List<User> findAll() {
@@ -86,7 +90,44 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User update(User user, long id) {
-        return null;
+
+        User currentUser = findById(id);
+        if(helperFunction.isAuthorizedToMakeChange(currentUser.getUsername())){
+            if(user.getUsername() != null){
+                currentUser.setUsername(user.getUsername().toLowerCase());
+            }
+            if(user.getPassword() != null){
+                currentUser.setPasswordNoEncrypt(user.getPassword());
+            }
+            if(user.getEmail() != null){
+                currentUser.setEmail(user.getEmail().toLowerCase());
+            }
+            if(user.getPhone() != null) {
+                currentUser.setPhone(user.getPhone());
+            }
+            if(user.getRoles().size() > 0){
+                currentUser.getRoles().clear();
+                for(UserRoles ur : user.getRoles()){
+                    Role addRole = roleService.findByRoleId(ur.getRole().getRoleid());
+                    currentUser.getRoles().add(new UserRoles(currentUser, addRole));
+                }
+            }
+            if(user.getUserplants().size() > 0){
+                currentUser.getUserplants().clear();
+                for (Plant p : user.getUserplants()){
+                    currentUser.getUserplants().add(new Plant(currentUser,
+                            p.getNickname(),
+                            p.getSpecies(),
+                            p.getImage(),
+                            p.getFrequency()));
+                }
+            }
+            return userrepos.save(currentUser);
+
+
+        }else{
+            throw new ResourceNotFoundException("THe user is not authorized to make change");
+        }
     }
 
     @Transactional
